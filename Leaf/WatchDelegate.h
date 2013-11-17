ce84fa4dfcbcfc6278ea5c57e99dd3b6c34d00a9
Phil <dplaxer25@aol.com>
2013-11-17 11:44:12 -0500

    Added Frameworks

---
+++ Leaf/WatchDelegate.h
@@ -0,0 +1,13 @@
+//
+//  WatchDelegate.h
+//  Leaf
+//
+//  Created by Phil Scarfi on 11/17/13.
+//  Copyright (c) 2013 Pioneer Mobile Applications. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <PebbleKit/PebbleKit.h>
+@interface WatchDelegate : NSObject
+
+@end
---
+++ Leaf/WatchDelegate.m
@@ -0,0 +1,13 @@
+//
+//  WatchDelegate.m
+//  Leaf
+//
+//  Created by Phil Scarfi on 11/17/13.
+//  Copyright (c) 2013 Pioneer Mobile Applications. All rights reserved.
+//
+
+#import "WatchDelegate.h"
+
+@implementation WatchDelegate
+
+@end
