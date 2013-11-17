9a41506e3b65fa65ee0daa19776eafab3b544c54
Phil <dplaxer25@aol.com>
2013-11-17 11:14:15 -0500

    Initial Commit

---
+++ Leaf/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Leaf
+//
+//  Created by Phil Scarfi on 11/17/13.
+//  Copyright (c) 2013 Pioneer Mobile Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Leaf/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Leaf
+//
+//  Created by Phil Scarfi on 11/17/13.
+//  Copyright (c) 2013 Pioneer Mobile Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
