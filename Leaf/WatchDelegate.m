6e89925a2002a863296966d602f379b52e7ddb24
Erik Risinger <erikrisinger@1x-vl953-72-19-69-34.wireless.umass.edu>
2013-11-17 16:15:43 -0500

    Wont accept updates

--- Leaf/WatchDelegate.m
+++ Leaf/WatchDelegate.m
@@ -9,6 +9,7 @@
 #import "WatchDelegate.h"
 #import "AppDelegate.h"
 @interface WatchDelegate(){
+    PBPebbleCentral *pebbleCentral;
      PBWatch *connectedWatch;
     AppDelegate *appDelegate;
 
@@ -20,13 +21,19 @@
 -(id)init{
     self = [super init];
     if (self) {
-        [[PBPebbleCentral defaultCentral] setDelegate:self];
+        pebbleCentral = [PBPebbleCentral defaultCentral];
+//        [[PBPebbleCentral defaultCentral] setDelegate:self];
+        [pebbleCentral setDelegate:self];
+        NSArray *connectedWatches = [pebbleCentral connectedWatches];
+        NSLog(@"%@", connectedWatches);
         uuid_t myAppUUIDbytes;
         NSUUID *myAppUUID = [[NSUUID alloc] initWithUUIDString:@"226834ae-786e-4302-a52f-6e7efc9f990b"];
         [myAppUUID getUUIDBytes:myAppUUIDbytes];
         
-        [[PBPebbleCentral defaultCentral] setAppUUID:[NSData dataWithBytes:myAppUUIDbytes length:16]];
-    }
+//        [[PBPebbleCentral defaultCentral] setAppUUID:[NSData dataWithBytes:myAppUUIDbytes length:16]];
+
+        [pebbleCentral setAppUUID:[NSData dataWithBytes:myAppUUIDbytes length:16]];
+        }
     return self;
 }
 
@@ -42,14 +49,32 @@
     
     if (connect) {
         //Connect
-        connectedWatch = [[PBPebbleCentral defaultCentral] lastConnectedWatch];
+        connectedWatch = [pebbleCentral lastConnectedWatch];
         [connectedWatch setDelegate:self];
-        NSLog(@"connected watch serial: %@", [connectedWatch serialNumber]);
+//        NSLog(@"connected watch serial: %@", [connectedWatch serialNumber]);
+        
+        [connectedWatch appMessagesGetIsSupported:^(PBWatch *watch, BOOL isAppMessagesSupported) {
+            if (isAppMessagesSupported) {
+                NSLog(@"This Pebble supports app message!");
+            }
+            else {
+                NSLog(@":( - This Pebble does not support app message!");
+            }
+        }];
+        NSNumber *key = @(0);
+        [connectedWatch appMessagesPushUpdate:[NSDictionary dictionaryWithObject:@"updated" forKey:key] onSent:^(PBWatch *watch, NSDictionary *update, NSError *error){
+            if (!error) {
+                NSLog(@"sent message");
+            }
+            else{
+                NSLog(@"got error %@", error);
+            }
+        }];
+
     }
     else{
         [connectedWatch closeSession:nil];
     }
-    
 }
 
 
@@ -59,6 +84,8 @@
     appDelegate= (AppDelegate *)[[UIApplication sharedApplication] delegate];
     appDelegate.FVC.connected=YES;
     [appDelegate.FVC.connectOutlet setTitle:@"Disconnect" forState:UIControlStateNormal];
+    
+    
 }
 
 - (void)pebbleCentral:(PBPebbleCentral*)central watchDidDisconnect:(PBWatch*)watch {
